158d347329c02fb01c40189060b07a2bd49319bd
ydhydhjanson <546150360@qq.com>
2015-11-03 17:53:48 +0800

    Revert "commit"
    This reverts commit ea7ecfbdd31d3de75c049323c497b83db9428a04.

--- build/run.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-package_name=web-project
-package_suffix=war
-package_path="${WORKSPACE}/target/${package_name}.${package_suffix}"
-target_path="${JENKINS_HOME}/build"
-#echo "package_path:${package_path}"
-cp ${package_path} ${target_path}
-./docker-compose.sh
